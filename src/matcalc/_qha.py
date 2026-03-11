abca89e09499234c428d253105f45c31152c6f23
Andrew S. Rosen <asrosen93@gmail.com>
2026-03-11 00:13:59 -0400

    Update _qha.py
    Signed-off-by: Andrew S. Rosen <asrosen93@gmail.com>

--- src/matcalc/_qha.py
+++ src/matcalc/_qha.py
@@ -105,7 +105,7 @@ class QHACalc(PropCalc):
         relax_structure: bool = True,
         relax_calc_kwargs: dict | None = None,
         phonon_calc_kwargs: dict | None = None,
-        scale_factors: Sequence[float] = tuple(np.arange(0.95, 1.05, 0.01).tolist()),
+        scale_factors: Sequence[float] = (0.95, 0.96, 0.97, 0.98, 0.99, 1.0, 1.01, 1.02, 1.03, 1.04, 1.05),
         imaginary_freq_tol: float = -0.01,
         on_imaginary_modes: Literal["error", "warn"] = "warn",
         fix_imaginary_attempts: int = 0,
